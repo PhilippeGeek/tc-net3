1448e1ca4314b87f74cc4e5d9193626619ec07d2
Philippe VIENNE <philippegeek@gmail.com>
2017-11-07 11:20:39 +0100

    Got a menu now!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  id: Observable<String>;
+  url: Observable<String>;
+
+  constructor(route: ActivatedRoute) {
+    console.log(route);
+  }
 }
