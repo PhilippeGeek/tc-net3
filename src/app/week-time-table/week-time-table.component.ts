2df6b88eec81dded1b3988ea6092b0ce5024dcbe
Philippe VIENNE <philippegeek@gmail.com>
2017-11-07 13:10:39 +0100

    Manage week number

--- src/app/week-time-table/week-time-table.component.ts
+++ src/app/week-time-table/week-time-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getDateOfISOWeek, getWeekNumber } from '../../utils/date';
 
 @Component({
   selector: 'app-week-time-table',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeekTimeTableComponent implements OnInit {
   weekDays = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
+  date: Date = new Date();
 
-  constructor() { }
+  constructor() {
+    const [year, week]: number[2] = getWeekNumber(this.date);
+    this.date = getDateOfISOWeek(week, year);
+  }
 
   ngOnInit() {
+  }
+  asWeek(date: Date): number {
+    return getWeekNumber(date)[1];
   }
 
 }
---
+++ src/utils/date.ts
@@ -0,0 +1,19 @@
+export function getDateOfISOWeek(w: number, y: number): Date {
+  const simple = new Date(y, 0, 1 + (w - 1) * 7);
+  const dow = simple.getDay();
+  const ISOweekStart = simple;
+  if (dow <= 4) {
+    ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+  } else {
+    ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+  }
+  return ISOweekStart;
+}
+
+export function getWeekNumber(d: Date): number[2] {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  const yearStart: Date = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  const weekNo: number = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+  return [d.getUTCFullYear(), weekNo];
+}
